c80f5483a96294190d36a3654290464ba0210348
Caitlin Stanton <livelaughlovelol4@yahoo.com>
2014-09-24 08:43:02 -0400

    Worked with Ethan Cheng
    Everything works!

--- 05-CodingBat/Driver.java
+++ 05-CodingBat/Driver.java
@@ -10,5 +10,10 @@ public class Driver {
 	System.out.println("...should be 17");
 	System.out.println(stringThing.diff21(24));
 	System.out.println("...should be 6");
+			System.out.println(x.lastDigit(24 , 39 ,9004));
+		System.out.println(x.nearHundred(209));
+		System.out.println(x.mixStart("twix"));
+		System.out.println(x.mixStart("mixStart"));
+		System.out.println(x.teaParty(11,4));
     }
 }
