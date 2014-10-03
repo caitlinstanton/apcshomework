8165fb440af06fb44b523387073e33aae318cbb7
Caitlin Stanton <livelaughlovelol4@yahoo.com>
2014-10-02 20:32:17 -0400

    Updated 07-Shapes

--- 07-Shapes/Driver.java
+++
@@ -1,8 +0,0 @@
-public class Driver {
-    
-    public static void main(String[] args) {
-	Shapes thing = new Shapes();
-	System.out.println(thing.tri1(3));
-	System.out.println(thing.tri2(5));
-    }
-}
