f6f08a55e4f883719eabe479368925e0b41bee65
Caitlin Stanton <livelaughlovelol4@yahoo.com>
2014-10-21 16:43:09 -0400

    Driver

---
+++ 12-Stuyablo/Driver.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+import java.io.*;  
+
+public class Driver {
+    public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	String name;
+	String enter; 
+	int i; 
+
+	System.out.println("Welcome, young traveler! What is your name?");
+	name = in.nextLine();
+
+	Mage m = new Mage();
+	Monster e = new Monster();
+
+	System.out.print("Hello " + name + "! Enter the world of GENERIC RPG FANTASY LAND");
+	for (i=1;i<3;i=i+1) {
+	try{
+	    Thread.sleep(1000);
+	} catch(Exception e){}
+	System.out.print(".");
+	}
+        try{
+	    Thread.sleep(1000);
+	} catch(Exception e){}
+	System.out.println(".");
+	try{
+	    Thread.sleep(1000);
+	} catch(Exception e){}
+	System.out.print("You wake up in a forest. (Hint: The 'enter' button is your friend.)");
+	enter = in.nextLine();
+        for (i=1;i<4;i=i+1) {
+	try{
+	    Thread.sleep(1000);
+	} catch(Exception e){}
+	System.out.print(".");
+	}
+	try{
+	    Thread.sleep(1000);
+	} catch(Exception e){}
+	System.out.print("Well, there are a lot of trees. I mean, it's a forest.");
+
+	enter = in.nextLine();
+        for (i=1;i<4;i=i+1) {
+	try{
+	    Thread.sleep(1000);
+	} catch(Exception e){}
+	System.out.print(".");
+	}
+	try{
+	    Thread.sleep(1000);
+	} catch(Exception e){}
+
+	System.out.println("What?!?!?!?! A strange traveler in the woods?!?!?!?! FIGHT HIM?!?!?! Get to it, " + name + "!");
+        enter = in.nextLine();
+    }
+}
