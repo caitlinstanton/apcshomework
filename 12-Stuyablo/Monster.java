64ad882ce06541ba00871aa21d404d5c8f25fb6c
Caitlin Stanton <livelaughlovelol4@yahoo.com>
2014-10-21 16:49:35 -0400

    Monster

---
+++ 12-Stuyablo/Monster.java
@@ -0,0 +1,40 @@
+import java.util.Random;
+
+public class Monster extends BaseChar {
+
+    public void claws(BaseChar other){
+	if Random.nextInt(10) <= 8 {
+		int damage = 7;
+		System.out.println(this + " swiped at " + other);
+		System.out.println(other + " lost "+ damage+ " HP.");
+	    }
+	else {
+	    System.out.println("Claws did not reach target...");
+	}
+    }
+
+    public void teeth(BaseChar other) {
+	if Random.nextInt(10) < 8 {
+		int damage = 10;
+		System.out.println(this + " savagely bit "+ other);
+		System.out.println(other + " lost "+ damage+ " HP.");
+	    }
+	else {
+	    System.out.println(other + " was out of biting reach...");
+	}
+    }
+	
+    public void wrath(BaseChar other) {
+	if Random.nextInt(10) < 7 {
+		int damage = 14;
+		System.out.println(this + " howled into the moonlight, summoniong a great evil upon "+ other);
+		System.out.println(other + "lost "+ damage+ " HP.");
+		if Random.nextInt(2)== 1 {
+			System.out.println(other + " was poisoned!");
+		    }
+	    }
+	else {
+	    System.out.println(other+ " blinded " + this + " with their beauty... attack missed.");
+	}
+    }
+}
