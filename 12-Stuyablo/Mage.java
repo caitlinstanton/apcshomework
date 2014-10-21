5080500cbb96523e61254282fb420a37ac247948
Caitlin Stanton <livelaughlovelol4@yahoo.com>
2014-10-21 08:13:15 -0400

    Updated

--- 12-Stuyablo/Mage.java
+++ 12-Stuyablo/Mage.java
@@ -6,35 +6,23 @@ public class Mage extends Basechar {
     Random thing = new Random();
     Scanner userInput = new Scanner(System.in);
     
-    public String spell1(Basechar other){
-	if(userInput == "spell1" && this.acc > */NUMBER HERE*/) {
-	    other.hp = other.hp - 10;
-	    this.mp = this.mp - 20;
-	    return(this.name + " shot a fireball at " + other.name);
-	    return(this.name + "'s hp is: " + this.hp);
-	    return(this.name + "'s  mp is: " + this.mp);
-	    return("Your opponent's hp is: " + other.hp);
-	}
-
-  
-    public String spell2(Basechar other){
-	if(userInput == "spell2" && this.acc > */NUMBER*/) {
-	    other.hp = other.hp - 10;
-	    this.mp = this.mp - 30;
-	    return(this.name + " electrified " + other.name);
-	    return(this.name + "'s hp is: " + this.hp);
-	    return(this.name + "'s  mp is: " + this.mp);
-	    return("Your opponent's hp is: " + other.hp);
-	}
-
- public String spell3(Basechar other){
-	if(userInput == "spell3" && this.acc > 80) {
-	    other.hp = other.hp - 20;
-	    this.mp = this.mp - 40;
-	    return(this.name + " electrified " + other.name);
-	    return(this.name + "'s hp is: " + this.hp);
-	    return(this.name + "'s  mp is: " + this.mp);
-	    return("Your opponent's hp is: " + other.hp);
+    public String spell(Basechar other){
+	if(userInput == "spell1" && r.nextInt(this.getACC()) > 20 ) {
+	    other.setHP(other.getHP() - 10);
+	    this.setMP(this.getMP() - 20);
+	    return(this + " shot a fireball at " + other);
+	   
+	}else if(userInput == "spell2" && r.nextInt(this.getACC()) > 0) {
+	    other.setHP(other.getHP - 10);
+	    this.setMP(this.getMP - 30);
+	    return(this + " electrified " + other);
+	}else if(userInput == "spell3" && r.nextInt(this.getACC()) > 20) {
+	    other.setHP(other.getHP() - 20);
+	    this.setMP(this.getMP() - 40);
+	    return(this + " froze " + other);
+	} else{
+	    this.setMP(this.getMP()-30);
+	    return "The spell fizzled and died";
 	}
 	
 
