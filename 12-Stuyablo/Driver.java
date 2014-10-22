1e759acc0f1390d49f7df8708a6acc9f52c02ace
Caitlin Stanton <livelaughlovelol4@yahoo.com>
2014-10-21 20:41:48 -0400

    Driver

--- 12-Stuyablo/Driver.java
+++ 12-Stuyablo/Driver.java
@@ -8,10 +8,11 @@ public class Driver {
 	Random r = new Random();
 	String name;
 	String enter; 
+	String spell;
 	int i; 
 	
 	int h = r.nextInt(13 - 7) + 7;
-	int m = r.nextInt(13 - 7) + 7;
+	int mp = r.nextInt(13 - 7) + 7;
 	int a = r.nextInt(13 - 7) + 7;
 	int d = r.nextInt(13 - 7) + 7;
 	int s = r.nextInt(13 - 7) + 7;
@@ -23,8 +24,8 @@ public class Driver {
 	System.out.println("Welcome, young traveler! What is your name?");
 	name = in.nextLine();
 
-	Mage m = new Mage(h, m, a, d, s, ac, c, n, w);
-	Monster e = new Monster(h, m, a, d, s, ac, c, n, w);
+	Mage m = new Mage(h, mp, a, d, s, ac, c, n, w);
+	Monster e = new Monster(h, mp, a, d, s, ac, c, n, w);
 
 	System.out.print("Hello " + name + "! Enter the world of GENERIC RPG FANTASY LAND");
 	for (i=1;i<3;i=i+1) {
@@ -66,5 +67,14 @@ public class Driver {
 
 	System.out.println("What?!?!?!?! A strange traveler in the woods?!?!?!?! FIGHT HIM?!?!?! Get to it, " + name + "!");
         enter = in.nextLine();
+
+	while (e.getHP() > 0){
+		System.out.println("How will you defeat this creature???");
+		System.out.println("Which spell will you use? Type out spell1, spell2, or spell3 to select");
+		spell = in.nextLine();
+		System.out.println(m.spell(e, spell));
+		System.out.println("Your MP is: " + m.getMP());
+		System.out.println("Your attacker's HP is: " + e.getHP());
+	}
     }
 }
