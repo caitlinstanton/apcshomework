b6e4bca61214f1bc0535b25c2fbd2b648939a96d
Your Name <you@example.com>
2014-11-07 08:29:00 -0500

    C

--- ClassWork/Arrays/Arrays.java
+++ ClassWork/Arrays/Arrays.java
@@ -1,16 +1,1 @@
-import java.util.*;
-import java.io.*;
-
-public class Arrays {
-
-    public int[] intArray() {
-	int[] Array100 = new int[100];
-	for (int i = 0; i < 100; i++) {
-	    Random r = new Random();
-	    int value = r.nextInt(150 - 75) + 75;
-	    Array100[i] = value;
-	}
-	return Array100;
-    }
-
-}
+public class Sarray {
---
+++ ClassWork/Arrays/SArray.java
@@ -0,0 +1,20 @@
+public class SArray {
+    //variableType of the list should be object to be general
+    private int[] data;
+
+    public SArray (){
+	//start array at size 10
+
+    }
+
+    /*
+    //add to the end
+    add(int i);
+    //add() at location index and shift everything else down
+    add(int index, int i);
+    //get
+    get(int i);
+    get(int index, int i);
+    size();
+    remove(int index);
+    */
