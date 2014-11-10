f9d85ab6f7158459b38e37fc02dbce47789e33a3
Your Name <you@example.com>
2014-11-10 08:26:43 -0500

    C

--- ClassWork/Errors/Driver.java
+++ ClassWork/Errors/Driver.java
@@ -1,8 +1,18 @@
 public class Driver{
     public static void main(String[] args) {
+	/*
 	int[] a = new int[5];
 	int[44] = 73;
 	int x = 10;
 	10/0;
+	*/
+	Eexample ee = new Eexample();
+	try {
+	    ee.etest(55);
+	    System.out.println("OK");
+	} catch (Exception e) {
+	    System.out.println("Error-we got: " + e);
+	}
+	System.out.println("After the exception");
     }
 }
