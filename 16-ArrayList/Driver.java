33382fb773d892032ee66acb100e33b2fa323347
Caitlin Stanton <livelaughlovelol4@yahoo.com>
2014-11-12 19:38:52 -0500

    Got rid of adjacent duplicates in an instance of ArrayList

--- 16-ArrayList/Driver.java
+++ 16-ArrayList/Driver.java
@@ -4,12 +4,22 @@ import java.util.*;
 public class Driver{
 
     public static void main(String[] args) {
-	ArrayList<Integer> ai;
-	ai = new ArrayList<Integer>();
+	ArrayList<Integer> ai = new ArrayList<Integer>();
 	Random r = new Random();
 	for (int i = 0; i < 20; i++){
 	    ai.add(r.nextInt(8));
 	}
+	System.out.println("Before checking for duplicates:");
+	System.out.println(ai);
+	int c = 0;
+	while (c < ai.size() - 1){
+	    if (ai.get(c) == ai.get(c + 1)){
+		ai.remove(c + 1);
+	    } else {
+		c++;
+	    }
+	}
+	System.out.println("After checking for duplicates:");
 	System.out.println(ai);
     }
 }
