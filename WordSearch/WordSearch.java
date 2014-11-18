04aa95a4d0f846da8bd78d8723bfec71453bcee9
Your Name <you@example.com>
2014-11-18 08:40:12 -0500

    C

--- WordSearch/WordSearch.java
+++ WordSearch/WordSearch.java
@@ -7,7 +7,7 @@ public class WordSearch {
     
     private char[][] board;
 
-    public WordSeard (int r, int c) {
+    public WordSearch (int r, int c) {
 	board = new char[r][c];
     }
 
@@ -18,7 +18,7 @@ public class WordSearch {
     public String toString(){
 	String ans = "";
 	for (int i = 0; i < board.length; i++){
-	    for (int j = 0; board[i].length; j++){
+	    for (int j = 0; j < board[i].length; j++){
 		board[i][j] = '.';
 		ans = ans + board[i][j];
 	    }
