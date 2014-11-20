92a691245a251b22e0fbe01800f3f7d6afd24f91
Caitlin Stanton <livelaughlovelol4@yahoo.com>
2014-11-20 17:37:21 -0500

    HW-18b--added two methods to check if a word can be printed horizontally or vertically and two methods to print words vertically (forwards and backwards), and updated the two methods to print words horizontally to take advantage of checkHorizontal()

--- 18-Wordsearch/WordSearch.java
+++ 18-Wordsearch/WordSearch.java
@@ -30,26 +30,32 @@ public class WordSearch{
 	return s;
     }
 
-    public void addWordHForward(String w,int row, int col){
-	boolean possible;
+    public boolean checkHorizontal(String w, int row, int col) {
+	boolean possible = true;
 	int r = row, c = col;
-	//checks to make sure row and col can fit in board
-	if (r <= board.length && col <= board[1].length) {
-	    possible = true;
-	    if (possible == true) {
-		for (int i = 0; i < w.length(); i++){
-		    //checks to see whether or not a word has already been printed there
-		    //stops if a letter is already at the selected position
-		    if (board[r][c+i] != '.'){
-			possible = false;
-			break;
-		    }
+	if (col + w.length() > board[0].length) {
+	    return false;
+	}
+	if (row > board.length) {
+	    return false;
+	}
+	for (int i = 0; i < w.length(); i++) {
+	    if (board[r][c] != '.') {
+		if (board[r][c] != w.charAt(i)) {
+		    return false;
 		}
 	    }
-	    if (possible = true) {
-		for (int i = 0; i < w.length() ; i++) {
-		    board[r][c+i] = w.charAt(i);
-		}
+	    c++;
+	}
+	return true;
+    }
+
+    public void addWordHForward(String w,int row, int col){
+	int r = row, c = col;
+	if (checkHorizontal(w, r, c) == true) {
+	    for (int i = 0; i < w.length() ; i++) {
+		board[r][c] = w.charAt(i);
+		c++;
 	    }
 	}
     }
@@ -60,12 +66,58 @@ public class WordSearch{
 	addWordHForward(newWord, row, col);
     }
 
+    public boolean checkVertical(String w, int row, int col) {
+	boolean possible = true;
+	int r = row, c = col;
+	if (row + w.length() > board.length) {
+	    return false;
+	}
+	if (col > board[0].length) {
+	    return false;
+	}
+	for (int i = 0; i < w.length(); i++) {
+	    if (board[r][c] != '.') {
+		if (board[r][c] != w.charAt(i)) {
+		    return false;
+		}
+	    }
+	    r++;
+	}
+	return true;
+    }
+
+     public void addWordVForward(String w,int row, int col){
+	int r = row, c = col;
+	if (checkVertical(w, r, c) == true) {
+	    for (int i = 0; i < w.length() ; i++) {
+		board[r+i][c] = w.charAt(i);
+	    }
+	}
+     }
+
+    public void addWordVBackward(String w, int row, int col){
+	//just flips the word given and prints it using the addWordHForward() method
+	String newWord = new StringBuilder(w).reverse().toString();
+	addWordVForward(newWord, row, col);
+    }
+
     public static void main(String[] args) {
 	WordSearch w = new WordSearch();
-	System.out.println(w);
 	w.addWordHForward("hello",3,5);
-	w.addWordHForward("look",7,8);
+	w.addWordHForward("hello",400,5);
+	w.addWordHForward("hello",3,655);
+
 	w.addWordHBackward("world",10,4);
+	w.addWordHBackward("world",100,4);
+	w.addWordHBackward("world",10,400);
+
+	w.addWordVForward("look",17,8);
+	w.addWordVForward("look",700,8);
+	w.addWordVForward("look",7,800);
+
+	w.addWordVBackward("great",15,4);
+	w.addWordVBackward("great",150,4);
+	w.addWordVBackward("great",10,04);
 				
 	System.out.println(w);
     }
