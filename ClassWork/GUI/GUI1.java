b48604e1e057d0bec8babce891f0c8146f626270
Your Name <livelaughlovelol4@yahoo.com>
2014-12-15 08:37:14 -0500

    GUI

---
+++ ClassWork/GUI/GUI1.java
@@ -0,0 +1,11 @@
+import java.swing.*;
+import java.awt.*;
+
+public class GUI1 extends JFrame{
+
+    public static void main(String[] args) {
+	GUI1 f = new GUI1();
+	f.setVisible(true);
+    }
+
+}
